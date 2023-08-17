7dcc0c9d759959eaeca2e121a95c8ef8d57d87a7
FabricioTff <fabriciotfferreira@gmail.com>
2023-08-17 14:59:49 -0300

    structure add

--- index.py
+++ index.py
@@ -10,6 +10,7 @@ import numpy as np
 
 
 # ========= App ============== #
+
 FONT_AWESOME = ["https://use.fontawesome.com/releases/v5.10.2/css/all.css"]
 dbc_css = "https://cdn.jsdelivr.net/gh/AnnMarieW/dash-bootstrap-templates@V1.0.4/dbc.min.css"
 
@@ -26,6 +27,18 @@ url_theme1 = dbc.themes.FLATLY
 url_theme2 = dbc.themes.VAPOR
 
 tab_card = {'height':"100%"}
+
+main_config = {
+    "hovermode": "x unified",
+    "legend": {"yanchor":"top", 
+                "y":0.9, 
+                "xanchor":"left",
+                "x":0.1,
+                "title": {"text": None},
+                "font" :{"color":"white"},
+                "bgcolor": "rgba(0,0,0,0.5)"},
+    "margin": {"l":0, "r":0, "t":10, "b":0}
+}
 
 
 # ===== Reading n cleaning File ====== #
@@ -88,7 +101,7 @@ app.layout = dbc.Container(children=[
                     dbc.Row([
                         dbc.Col([
                             html.H3("Máximos e Mínimos"),
-                            dcc.Graph(id = "static-maxmin", config={'displayModeBar': False, 'showTips': False})
+                            dcc.Graph(id = "static_maxmin", config={'displayModeBar': False, 'showTips': False})
                         ])
                     ])
                 ])
@@ -128,12 +141,12 @@ app.layout = dbc.Container(children=[
                         ], sm = 12, md= 6),
                         dbc.Col([
                             dcc.Graph(id='estadobar_graph', config={'displayModeBar': False, 'showTips': False})
-                        ], sm = 12, md= 6),
-                    ])
+                        ], sm = 12, md= 6)
+                    ],style= {"column-gap":"0px"})
                 ])
-            ])
-        ])
-    ]),
+            ],style= tab_card)
+        ],sm=12 , lg = 7)
+    ],className= "g-2 my-auto"),
     
     ###### Row 2 #######
     
@@ -144,16 +157,28 @@ app.layout = dbc.Container(children=[
                     dbc.Row([
                         dbc.Col([
                             html.H3("Preço x Estado"),
-                            html.H6("Comparação temporal entre estados")
+                            html.H6("Comparação temporal entre estados"),
+                            dbc.Row([
+                                dbc.Col([
+                                    dcc.Dropdown(
+                                        id = "select_estado0",
+                                        className= 'dbc',
+                                        value= df_main.at[df_main.index[1], "ESTADO"],
+                                        options= [{"label": x , "value": x } for x in df_main["ESTADO"].unique()],
+                                        multi = True
+                                    )
+                                ], sm = 10)
+                            ])
+                            
                         ]),
-                    dbc.Row([
-                        dbc.Col([
-                            dcc.Graph(id = "preco_estado", config={'displayModeBar': False, 'showTips': False})
+                        dbc.Row([
+                            dbc.Col([
+                                dcc.Graph(id = "animation_graph", config={'displayModeBar': False, 'showTips': False})
+                            ])
                         ])
                     ])
-                    ])
                 ])
-            ])
+            ],style=tab_card)
         ], sm = 5),
         dbc.Col([
             dbc.Card([
@@ -171,55 +196,107 @@ app.layout = dbc.Container(children=[
                                         value= df_main.at[df_main.index[1], "ESTADO"],
                                         options= [{"label": x , "value": x } for x in df_main["ESTADO"].unique()]
                                     )
-                                ], sm = 6),
+                                ], sm = 10,  md = 6),
                                 dbc.Col([
                                     dcc.Dropdown(
                                         id = "select_estado2",
                                         clearable= False,
                                         className= 'dbc',
-                                        value= df_main.at[df_main.index[2], "ESTADO"],
+                                        value= df_main.at[df_main.index[3], "ESTADO"],
                                         options= [{"label": x , "value": x } for x in df_main["ESTADO"].unique()]
                                     )
-                                ], sm = 6),
+                                ], sm = 10, md = 6),
                                 dbc.Row([
                                     dbc.Col([
-                                        dcc.Graph(id = "compare_graph", config={'displayModeBar': False, 'showTips': False})
+                                        dcc.Graph(id = "direct_comparison_graph", config={'displayModeBar': False, 'showTips': False})
                                     ])
-                                ])
+                                ]),
+                                html.P(id="desc_comparison", style={"color": "gray", "font-size":"80%"})
                             ])
                         ])
                     ])
                 ])
-            ])
-        ], sm = 4),
+            ],style=tab_card)
+        ], sm = 12, md = 6, lg = 4),
+        
         dbc.Col([
+            
             dbc.Row([
                 dbc.Col([
                     dbc.Card([
                         dbc.CardBody([
-                            html.H4("Teste"),
+                            dcc.Graph(id="card1_indicators", config={'displayModeBar': False, 'showTips': False}, style= {"margin-top": "30px"})
                         ])
-                    ])
+                    ],style=tab_card)
                 ])
-            ]),
+            ], justify='center', style={'padding-bottom': '7px', 'height': '50%'}),
+            
             dbc.Row([
                 dbc.Col([
                     dbc.Card([
                         dbc.CardBody([
-                            html.H4("Teste2")
+                            dcc.Graph(id="card2_indicators", config={'displayModeBar': False, 'showTips': False}, style= {"margin-top": "30px"})
                         ])
-                    ])
+                    ],style=tab_card)
                 ])
-            ])
-        ])
-    ])
-
-
-
+            ], justify='center', style={'height': '50%'})
+            
+        ],sm=12, lg = 3,style={"height": "100%"})
+    ],className= "g-2 my-auto"),
+       
+    dbc.Row([
+        dbc.Col([
+            dbc.Card([
+                dbc.Row([
+                    dbc.Col([
+                        dbc.Button([html.I(className="fa fa-play")], id="play-button", style={"margin-right": "15px"}),
+                        dbc.Button([html.I(className="fa fa-stop")], id="stop-button"),
+                    ], sm= 12, md = 1, style={"justify-content":"center", "margin-top":"10px"}),
+                    dbc.Col([
+                        dcc.RangeSlider(
+                            id="rangeslider",
+                            marks = {int(x): f"{x}" for x in df_main["ANO"].unique()},
+                            step= 3,
+                            min=int(df_main["ANO"].min()),
+                            max=int(df_main["ANO"].max()),
+                            value = [int(df_main["ANO"].min()),int(df_main["ANO"].max())],
+                            dots= True,
+                            pushable= 3,
+                            tooltip= {"always_visible": False, "placement": "bottom"}
+                        )
+                    ],sm = 12, md = 10, style= {"margin-top": "15px"}),
+                    dcc.Interval(id="interval", interval = 2000)
+                ], class_name='g-1', style={"height": "20%", "justify-content":"center"})
+            ],style= tab_card)
+        ])        
+    ],className= "g-2 my-auto")
+    
 ], fluid=True, style={'height': '100%'})
 
 
 # ======== Callbacks ========== #
+
+@app.callback(
+    Output("static_maxmin", "figure"),
+    [Input("dataset", "data"),
+    Input(ThemeSwitchAIO.ids.switch("theme"), "value")]
+)
+def func(data, toggle):
+    template = template_theme1 if toggle else template_theme2
+    
+    dff = pd.DataFrame(data), 
+    min = dff.groupby(["ANO"])["VALOR REVENDA (R$/L)"].min()
+    max = dff.groupby(["ANO"])["VALOR REVENDA (R$/L)"].max()
+
+    final_df = pd.concat([max, min], axis = 1)
+    final_df.columns = ["Máximo", "Mínimo"]
+    
+    fig = px.line(final_df, x = final_df.index, y = final_df.columns, template=template)
+    
+    fig.update.layout(main_config, height = 150, xaxis_title = None, yaxis_title = None)
+    
+    return fig
+
 
 
 # Run server
